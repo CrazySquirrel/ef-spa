1713385fa77056c3a792dd3c35ddeb06bf570fb2
Sergei Iastrebov <info@crazysquirrel.ru>
2018-12-16 01:11:31 +0300

    TASK-1: utils

---
+++ utils/index.js
@@ -0,0 +1,23 @@
+const os = require('os');
+
+// Get current os ip address
+function getCurrentIP() {
+  const ifaces = os.networkInterfaces();
+
+  for (const ifname in ifaces) {
+    if (ifaces.hasOwnProperty(ifname)) {
+      for (const iface of ifaces[ifname]) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+          // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+          continue;
+        }
+
+        return iface.address;
+      }
+    }
+  }
+}
+
+module.exports = {
+  getCurrentIP
+};
