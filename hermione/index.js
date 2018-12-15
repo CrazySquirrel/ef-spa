9f19f7cc217233c5850b27cd4fb5407303ea1534
Sergei Iastrebov <info@crazysquirrel.ru>
2018-12-16 02:05:58 +0300

    TASK-2: Hermione

--- hermione/index.js
+++ hermione/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const {spawn, spawnSync} = require('child_process');
 
+const { getCurrentIP } = require('../utils');
+
+console.log(getCurrentIP());
+
 const server = spawn('node', ['server.js'], {
   cwd: path.resolve(__dirname, '../'),
   checkCWD: true,
@@ -9,19 +13,29 @@ const server = spawn('node', ['server.js'], {
 });
 
 const steps = [
+  'node ./hermione/wait-for-server.js',
   'docker-compose -f docker-compose-selenium.yml up -d --force-recreate',
   'node ./hermione/wait-for-grid.js',
   `./node_modules/.bin/hermione ${process.argv.slice(2).join(' ')}`,
   'docker-compose -f docker-compose-selenium.yml down'
 ];
 
+let code = 0;
+
 for (const step of steps) {
-  spawnSync(step, {
+  const {error} = spawnSync(step, {
     cwd: path.resolve(__dirname, '../'),
     checkCWD: true,
     stdio: 'inherit',
     shell: true
   });
+
+  if (error) {
+    code = 1;
+    break;
+  }
 }
 
 server.kill();
+
+process.exit(code);
--- hermione/wait-for-server.js
+++ hermione/wait-for-server.js
@@ -1,23 +1,25 @@
 const http = require('http');
 const process = require('process');
 
+const serverConfig = require('../server.config.json');
+
+process.env.NODE_ENV = serverConfig.apps[0].env.NODE_ENV || 'production';
+
+const PORTS = serverConfig.apps[0][process.env.NODE_ENV].PORTS;
+
 function wait() {
   return new Promise((resolve, reject) => {
-    console.log('Waiting for the Grid');
+    console.log('Waiting for the Server');
 
     setTimeout(() => {
-      http.get('http://localhost:4444/wd/hub/status', (resp) => {
+      http.get(`http://localhost:${PORTS.HTTP}/`, (resp) => {
         let data = '';
 
         resp.on('data', (chunk) => data += chunk);
 
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
-          if (JSON.parse(data).value.ready !== true) {
-            reject(new Error('Not ready'));
-          } else {
-            resolve();
-          }
+          resolve();
         });
 
       }).on('error', reject);
@@ -27,5 +29,5 @@ function wait() {
 }
 
 wait()
-.then(() => console.log('Selenium Grid is up'))
+.then(() => console.log('Server is up'))
 .then(() => process.exit(0));
